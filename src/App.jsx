f14d660b986a8599d2f0da9b94aa802f9669818a
Hemant Mistry <hemant.mistry101@gmail.com>
2023-10-16 23:34:47 +0530

    Phase 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import AllPosts from "./Components/AllPosts/AllPosts";
+import MainContent from "./Components/MainContent/MainContent";
+
+
+function App() {
+  return (
+    <>
+    <MainContent/>
+    <AllPosts/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/AllPosts/AllPosts.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./AllPosts.css";
+export default function AllPosts() {
+  return (
+    <div>
+      <br></br>
+      <div class="card">
+        <div class="card-header">Genre: Sci-fi</div>
+        <div class="card-body">
+          <h5 class="card-title">
+            With supporting text below as a natural lead-in to additional
+            contentith supporting text below as a natural lead-in to additional
+            content
+          </h5>
+          <p class="card-text">- to additional content.</p>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header">Genre: Sci-fi</div>
+        <div class="card-body">
+          <h5 class="card-title">
+            With supporting text below as a natural lead-in to additional
+            contentith supporting text below as a natural lead-in to additional
+            content
+          </h5>
+          <p class="card-text">- to additional content.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
